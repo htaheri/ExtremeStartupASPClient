1721f381dc2a1604d3c82ae78339e8e2e8eb9fdb
hossein taheri <hossein@tahey.com>
2017-07-11 17:09:11 +0100

    add SignalR

--- NancyAspNetHost1/Modules/IndexModule.cs
+++ NancyAspNetHost1/Modules/IndexModule.cs
@@ -6,13 +6,17 @@ namespace NancyAspNetHost1.Modules
 {
     public class IndexModule : NancyModule
     {
+        static string GlobalLog;
         public IndexModule()
         {
-			Get["/Report"] = parameters =>
+			Get["/Home"] = parameters =>
 			{
 				return View["index"];
 			};
-			
+			Get["/Report"] = parameters =>
+            {
+	            return View["report"];
+            };
             Get["/"] = parameters => { return HandleQuery(parameters); };
 
         }
@@ -29,13 +33,15 @@ namespace NancyAspNetHost1.Modules
 
         private void WriteToLog(string log)
         {
-            string path = @"Content/request.log";
+            GlobalLog += "\\r\\n" + log;
+            //string path = @"Content/request.log";
 
-			using (StreamWriter sw = File.AppendText(path))
-			{
-				sw.WriteLine(log);
+			//using (StreamWriter sw = File.AppendText(path))
+			//{
+			//	sw.WriteLine(log);
 
-			}
+			//}
+
 			System.Diagnostics.Debug.WriteLine(log);
 			//System.IO.File.WriteAllText(@"Content/request.log", log);
         }
---
+++ NancyAspNetHost1/UpdateHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace NancyAspNetHost1
+{
+    public class UpdateHub: Hub
+    {
+		public void Send(string name, string message)
+		{
+			// Call the broadcastMessage method to update clients.
+			Clients.All.broadcastMessage(name, message);
+		}
+    }
+}
