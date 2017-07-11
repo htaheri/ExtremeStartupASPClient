6c1568f3616c2d5330854451a11eac01756d1800
hossein taheri <hossein@tahey.com>
2017-07-11 13:22:15 +0100

    Write log

--- NancyAspNetHost1/Modules/IndexModule.cs
+++ NancyAspNetHost1/Modules/IndexModule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Nancy;
 
 namespace NancyAspNetHost1.Modules
@@ -7,24 +8,36 @@ namespace NancyAspNetHost1.Modules
     {
         public IndexModule()
         {
-            Get["/"] = parameters => { return handleQuery(parameters); };
-
-            //Get["/"] = parameters =>
-            //{
-            //    return View["index"];
-            //};
-
+			Get["/Report"] = parameters =>
+			{
+				return View["index"];
+			};
+			
+            Get["/"] = parameters => { return HandleQuery(parameters); };
 
         }
-        private String handleQuery(dynamic parameters)
+
+        private String HandleQuery(dynamic parameters)
         {
             String query = "Query: ";
             query += Request.Url.Query;
 
-            System.Diagnostics.Debug.WriteLine(query);
-            //Debug.WriteLine(query);
+
+            WriteToLog(query) ;
             return query;
         }
 
+        private void WriteToLog(string log)
+        {
+            string path = @"Content/request.log";
+
+			using (StreamWriter sw = File.AppendText(path))
+			{
+				sw.WriteLine(log);
+
+			}
+			System.Diagnostics.Debug.WriteLine(log);
+			//System.IO.File.WriteAllText(@"Content/request.log", log);
+        }
     }
 }
