ed48cf871f0a798e78d2dbcec408bd3b93d8318b
hossein taheri <hossein@tahey.com>
2017-07-11 03:21:00 +0100

    add files

---
+++ NancyAspNetHost1/Modules/IndexModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Nancy;
+
+namespace NancyAspNetHost1.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters => { return handleQuery(parameters); };
+
+            //Get["/"] = parameters =>
+            //{
+            //    return View["index"];
+            //};
+
+
+        }
+        private String handleQuery(dynamic parameters)
+        {
+            String query = "Query: ";
+            query += Request.Url.Query;
+
+            System.Diagnostics.Debug.WriteLine(query);
+            //Debug.WriteLine(query);
+            return query;
+        }
+
+    }
+}
